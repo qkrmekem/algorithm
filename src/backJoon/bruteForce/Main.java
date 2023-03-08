76a8070bf300c50d0ae2c834e365e4f5710a3366
qkrmekem <magicofclown@naver.com>
2023-03-08 23:42:04 +0900

    완전탐색 백준 2231 분해합 백준 7568 덩치

--- src/backJoon/bruteForce/Main.java
+++ src/backJoon/bruteForce/Main.java
@@ -5,34 +5,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        int result = d(num);
-        System.out.println(result);
+        System.out.println(123%10);
     }
 
-    private static int d(int num) {
-        if (num < 100){
-            return num;
-        } else if (num == 1000) {
-            return d(999);
-        } else {
-            int cnt = 99;
-            for (int i = 100; i <= num; i++){
-                int tmp = i;
-                //1의 자리 수
-                int a = tmp % 10;
-                tmp /= 10;
-                //10의 자리 수
-                int b = tmp % 10;
-                tmp /= 10;
-                //100의 자리 수
-                int c = tmp % 10;
-                if((a-b) == (b-c)){
-                    cnt++;
-                }
-            }
-            return cnt;
-        }
-    }
+
 }
