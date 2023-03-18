47e9e4f1fecaa6466cc1c3d0bb70d229b1ecc39c
qkrmekem <magicofclown@naver.com>
2023-03-19 03:11:10 +0900

    프로그래머스 무인도 여행 혼자 풀어보기

--- src/programmers/Travel.java
+++ src/programmers/Travel.java
@@ -2,76 +2,81 @@ package programmers;
 
 import java.util.*;
 
-public class Travel {
-    static int dx[] = {-1,1,0,0};
-    static int dy[] = {0,0,-1,1};
+class Travel {
 
+    // 무인도 여행
 
-    static boolean visited[][];
+    static boolean[][] visited;
     static char[][] map;
-    static ArrayList<Integer> answer = new ArrayList<>();
+
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
 
     public Integer[] solution(String[] maps) {
-        visited = new boolean[maps.length][maps[0].length()];
+        int[] answer = {};
 
         map = new char[maps.length][maps[0].length()];
+        visited = new boolean[maps.length][maps[0].length()];
 
-        for(int i=0; i<maps.length; i++){
-            for(int j=0; j<maps[0].length(); j++){
-                map[i][j] = maps[i].charAt(j);
+        List<Integer> list = new ArrayList<>();
+
+        for(int i = 0; i< maps.length; i++){
+            for(int y = 0; y < maps[i].length(); y++){
+                map[i][y] = maps[i].charAt(y);
             }
         }
 
-        for(int i=0; i<maps.length; i++){
-            for(int j=0; j<maps[0].length(); j++){
-                if(!visited[i][j] && maps[i].charAt(j) != 'X'){
-                    bfs(new Pos(i, j));
+        for(int i = 0; i < map.length; i++){
+            for(int y = 0; y < map[0].length; y++){
+                if(visited[i][y] == false && map[i][y] != 'X'){
+                    list.add(bfs(new Pos(i, y)));
                 }
             }
         }
 
-        Collections.sort(answer);
+        Collections.sort(list);
 
-        if(answer.size() == 0) answer.add(-1);
+        if(list.size() == 0){
+            list.add(-1);
+        }
 
-        return answer.toArray(new Integer[0]);
+        return list.toArray(new Integer[0]);
     }
-    public static void bfs(Pos start){
+
+    public static int bfs(Pos start){
         Queue<Pos> q = new LinkedList<>();
         q.add(start);
         visited[start.x][start.y] = true;
 
         int sum = 0;
-        while(!q.isEmpty()){
+        while(q.isEmpty() == false){
             Pos cur = q.poll();
 
-            sum += map[cur.x][cur.y] - '0';
-            for(int i=0; i<4; i++){
+            sum += map[cur.x][cur.y]-'0';
+            for(int i = 0; i < dx.length; i++){
                 int nx = cur.x + dx[i];
                 int ny = cur.y + dy[i];
 
-                if(check(nx,ny) && !visited[nx][ny]){
-                    q.add(new Pos(nx,ny));
+                if(check(nx, ny) && map[nx][ny] != 'X'){
+                    q.add(new Pos(nx, ny));
                     visited[nx][ny] = true;
                 }
             }
         }
-        answer.add(sum);
-
-
+        return sum;
     }
 
     public static boolean check(int nx, int ny){
-        return (nx >=0 && nx < map.length && ny >=0 && ny <map[0].length
-                && map[nx][ny] != 'X') ? true : false;
+        if(nx >= 0 && nx < map.length && ny >= 0 && ny < map[0].length && visited[nx][ny] == false){
+            return true;
+        }else return false;
     }
-
 }
 
 class Pos{
-    int x, y;
-
-    public Pos(int x, int y) {
+    int x;
+    int y;
+    public Pos(int x, int y){
         this.x = x;
         this.y = y;
     }
