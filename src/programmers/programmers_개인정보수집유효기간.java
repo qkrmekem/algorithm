854716f9c3f135a39b8e1d4adc6a3be8f04c1bcf
qkrmekem <magicofclown@naver.com>
2023-03-24 00:45:23 +0900

    프로그래머스 개인정보 수집 유효기간 완료

--- src/programmers/programmers_개인정보수집유효기간.java
+++ src/programmers/programmers_개인정보수집유효기간.java
@@ -3,26 +3,39 @@ package programmers;
 import java.util.*;
 
 public class programmers_개인정보수집유효기간 {
-    public int[] solution(String today, String[] terms, String[] privacies) {
-        int[] answer = new int[privacies.length];
+    public Integer[] solution(String today, String[] terms, String[] privacies) {
+        Integer[] answer = {};
+        List<Integer> list = new ArrayList<>();
 
-        Map map = new HashMap<>();
+        Map<String,Integer> map = new HashMap<>();
         for(int i = 0; i < terms.length; i++){
             String tmp = terms[i].split(" ")[0];
-            String term = terms[i].split(" ")[1];
+            Integer term = Integer.parseInt(terms[i].split(" ")[1]);
             map.put(tmp, term);
         }
 
+        Integer toDayYear = Integer.parseInt(today.split("\\.")[0])*12*28;
+        Integer toDayMonth = Integer.parseInt(today.split("\\.")[1])*28;
+        Integer toDayDay = Integer.parseInt(today.split("\\.")[2]);
+
+        Integer toDayTotal = toDayYear + toDayMonth + toDayDay;
+
         for(int y = 0; y < privacies.length; y++){
-            String days = privacies[y].split(" ")[0].replace(".", "");
+            String tmpEx = privacies[y].split(" ")[0];
+            Integer years = Integer.parseInt(privacies[y].split(" ")[0].split("\\.")[0])*12*28;
+            Integer months = Integer.parseInt(privacies[y].split(" ")[0].split("\\.")[1])*28;
+            Integer days = Integer.parseInt(privacies[y].split(" ")[0].split("\\.")[2]);
             String ttmp = privacies[y].split(" ")[1];
 
-            Integer dayInt = Integer.parseInt(days);
-            term = map.get(ttmp);
-            System.out.println(days);
+            int term = map.get(ttmp)*28;
+
+            Integer expireDay = years + months + days + term;
+            if(toDayTotal >= expireDay){
+                list.add(y+1);
+            }
         }
 
-
+        answer = list.toArray(new Integer[0]);
 
 
         return answer;
