80ded0ff2c64bb6fe05d288d003180ac7bb34270
qkrmekem <magicofclown@naver.com>
2023-06-09 23:41:07 +0900

    구간 합 구하기

--- src/backJoon/Q1546_평균구하기.java
+++ src/backJoon/Q1546_평균구하기.java
@@ -8,16 +8,18 @@ public class Q1546_평균구하기 {
         int quantity = sc.nextInt();
         int[] arr = new int[quantity];
         int max = 0;
+        float sum = 0;
         for (int i = 0; i < quantity; i++) {
             arr[i] = sc.nextInt();
             if (arr[i] > max){
                 max=arr[i];
             }
+
         }
-        float sum = 0;
+//        System.out.println(sum * 100.0 / max / quantity);
         for (int i = 0; i < quantity; i++){
-            float tmp = Float.valueOf(arr[i]) / Float.valueOf(max) * 100.0f;
-            System.out.println(tmp);
+            float tmp = arr[i] / Float.valueOf(max) * 100;
+//            System.out.println(tmp);
             sum += tmp;
         }
         System.out.println(sum / quantity);
