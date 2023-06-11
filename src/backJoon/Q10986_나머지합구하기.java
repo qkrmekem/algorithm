8a67bb7845169113919122ac417df7a874f948f9
qkrmekem <magicofclown@naver.com>
2023-06-12 03:25:50 +0900

    투포인터-수들의합

--- src/backJoon/Q10986_나머지합구하기.java
+++ src/backJoon/Q10986_나머지합구하기.java
@@ -34,21 +34,29 @@ public class Q10986_나머지합구하기 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
+        // 구간합
         long[] s = new long[n];
+        // 나머지 값이 같은 갯수를 누적할 배열
         long[] c = new long[m];
         long answer = 0;
+        // 구간 합 구하기
         s[0] = sc.nextInt();
         for (int i = 1; i < n; i++){
             s[i] = s[i - 1] + sc.nextInt();
         }
+        // 나머지 값 구하기
         for (int i = 0; i < n; i++){
             int remainder = (int) (s[i] % m);
+            // 나머지가 0일때
             if (remainder==0){
                 answer++;
             }
+            // 나머지가 같은 인덱스에 개수 누적
             c[remainder]++;
         }
         for (int i = 0; i < m; i++) {
+            // 나머지가 같은 인덱스가 2 이상일 경우
+            // 조합의 경우의 수를 구함
             if (c[i] > 1) {
                 answer = answer + (c[i] * (c[i]-1) / 2);
             }
