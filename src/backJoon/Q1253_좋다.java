69b104e3d770b07c09852f63478413e9941d6bab
qkrmekem <magicofclown@naver.com>
2023-06-15 00:15:09 +0900

    투포인터-좋은수

--- src/backJoon/Q1253_좋다.java
+++ src/backJoon/Q1253_좋다.java
@@ -1,21 +1,24 @@
 package backJoon;
 
+import java.io.*;
 import java.util.*;
 
 public class Q1253_좋다 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer token = new StringTokenizer(bf.readLine());
+        int n = Integer.parseInt(token.nextToken());
         long[] arr = new long[n];
+        token = new StringTokenizer(bf.readLine());
         for (int i = 0; i < n; i++){
-            arr[i] = sc.nextLong();
+            arr[i] = Long.parseLong(token.nextToken());
         }
         Arrays.sort(arr);
 
         int count = 0;
         for (int i = 0; i < n; i++){
             int start = 0;
-            int end = i-1;
+            int end = n-1;
             long sum = arr[i];
             while (start<end){
                 if (arr[start] + arr[end] == sum){
@@ -27,13 +30,14 @@ public class Q1253_좋다 {
                     } else if (end == i) {
                         end--;
                     }
-                }else if(arr[start] + arr[end] > sum){
-                    end--;
-                }else {
+                }else if(arr[start] + arr[end] < sum){
                     start++;
+                }else {
+                    end--;
                 }
             }
         }
         System.out.println(count);
+        bf.close();
     }
 }
