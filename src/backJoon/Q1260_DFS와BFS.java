e5fd014d52ea1f6c980238b2e1c2f26d1a739b17
qkrmekem <magicofclown@naver.com>
2023-06-25 23:59:06 +0900

    bfs - dfs와bfs

---
+++ src/backJoon/Q1260_DFS와BFS.java
@@ -0,0 +1,68 @@
+package backJoon;
+
+import java.io.*;
+import java.util.*;
+
+public class Q1260_DFS와BFS {
+
+    static ArrayList<Integer>[] arr;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[n+1];
+        arr = new ArrayList[n+1];
+
+        for (int i = 1; i <= n; i++) {
+            arr[i] = new ArrayList<>();
+        }
+
+        for (int i = 1; i <= m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            arr[s].add(e);
+            arr[e].add(s);
+        }
+
+        for (int i = 1; i <= n; i++) {
+            Collections.sort(arr[i]);
+        }
+        dfs(v);
+        visited = new boolean[n+1];
+        System.out.println();
+        bfs(v);
+        System.out.println();
+    }
+
+    public static void dfs(int node) {
+        System.out.print(node + " ");
+        visited[node] = true;
+        for (int i : arr[node]) {
+            if (!visited[i]) {
+                dfs(i);
+            }
+        }
+    }
+
+    public static void bfs(int node) {
+        System.out.println(node + " ");
+        Queue<Integer> q = new LinkedList<>();
+        q.add(node);
+        visited[node] = true;
+
+        while (!q.isEmpty()) {
+            int now = q.poll();
+            System.out.println(now + " ");
+            for (int i : arr[now]) {
+                visited[i] = true;
+                q.add(i);
+            }
+        }
+    }
+}
